e9c10a4fd865c966a7dd833153b6bd5640d14593
KaiN <kain@piwnica.ws>
2017-11-30 08:50:49 +0100

    Added serial cmd parsing

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,15 @@
 #include "pins.h"
 #include "steppers.h"
 
+void serPrintf(const char *szFmt, ...) {
+	char szBfr[50];
+	va_list vArgs;
+	va_start(vArgs, szFmt);
+	vsprintf(szBfr, szFmt, vArgs);
+	va_end(vArgs);
+	Serial.print(szBfr);
+}
+
 void setup() {
 	// put your setup code here, to run once:
 
@@ -19,19 +28,31 @@ void setup() {
 	digitalWrite(SAFETY_PIN, HIGH);
 
 	stepperSetup();
-	Serial.print("dupa");
+	Serial.print("HELO");
 }
 
 int lMaxX, lMaxY, lMaxZ, lSafety;
 int s_steps = 0;
 
-void serPrintf(const char *szFmt, ...) {
-	char szBfr[50];
-	va_list vArgs;
-	va_start(vArgs, szFmt);
-	vsprintf(szBfr, szFmt, vArgs);
-	va_end(vArgs);
-	Serial.print(szBfr);
+char s_szBfr[30];
+uint8_t s_ubBfrLen = 0;
+
+uint8_t cmdInterpret(void) {
+	int16_t uwTmpX, uwTmpY, uwTmpZ;
+
+	// No data - skip
+	if(!s_ubBfrLen)
+		return 0;
+
+	// Got correct data?
+	if(sscanf(s_szBfr, "x %d y %d z %d", &uwTmpX, &uwTmpY, &uwTmpZ) == 3) {
+		serPrintf("x: %d, y: %d, z: %d\r\n", uwTmpX, uwTmpY, uwTmpZ);
+		return 1;
+	}
+
+	// Got wrong data
+	Serial.write("ERR(parse)\r\n");
+	return 0;
 }
 
 void loop() {
@@ -42,17 +63,21 @@ void loop() {
 	lMaxY = digitalRead(Y_MAX_PIN);
 	lMaxZ = digitalRead(Z_MAX_PIN);
 
-	// if(lSafety) {
-	// 	stepperStop();
-	// 	return;
-	// }
-	// stepperStart();
-
 	while(Serial.available()) {
-		int lRead = Serial.parseInt();
-		if(lRead > 0)
-			s_steps = lRead;
+		s_szBfr[s_ubBfrLen++] = Serial.read();
+		if(s_szBfr[s_ubBfrLen-1] == '\r' || s_szBfr[s_ubBfrLen-1] == '\n') {
+			s_szBfr[s_ubBfrLen-1] = 0; // Make string null-terminated
+			cmdInterpret();
+			s_ubBfrLen = 0;
+		}
+		s_steps = 0;
 	}
+
+	if(lSafety) {
+		stepperStop();
+		return;
+	}
+	stepperStart();
 
 	if(s_steps > 0) {
 		uint8_t ubMotors = 0;
