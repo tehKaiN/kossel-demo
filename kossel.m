d5629ee32eb0525e53a3566020fb2e76433ab827
KaiN <kain@piwnica.ws>
2018-01-04 07:32:20 +0100

    MATLAB/Octave script to drive Kossel printer

---
+++ kossel.m
@@ -0,0 +1,77 @@
+clf;
+armcnt = 3;
+Rarms = 3;
+Rpret = 4;
+h = 7;
+
+% s = serial('COM5', 'baudrate', 9600);
+% fopen(s);
+
+
+% Polozenie koncowki drukujacej
+xc = 0;
+yc = 0;
+zc = 0;
+
+for i = 1:3
+	xbase(i) = Rarms * sin((2*pi*i)/armcnt);
+	ybase(i) = Rarms * cos((2*pi*i)/armcnt);
+end
+
+for i = 1:3
+	dx = xbase(i) - xc;
+	dy = ybase(1) - yc;
+	z(i) = zc + sqrt(Rpret*Rpret - dx*dx - dy*dy);
+	arms(i) = line([xbase(i), xc], [ybase(i), yc], [z(i), zc]);
+	set(arms(i), 'Color', [.7, 0, 0]);
+end
+
+% z(i) - pozycje kolumn
+
+colLines = zeros(3);
+bottomLines = zeros(3);
+topLines = zeros(3);
+
+bottomLines(1) = line([xbase(1), xbase(2)], [ybase(1), ybase(2)], [0, 0]);
+bottomLines(1) = line([xbase(2), xbase(3)], [ybase(2), ybase(3)], [0, 0]);
+bottomLines(1) = line([xbase(3), xbase(1)], [ybase(3), ybase(1)], [0, 0]);
+
+topLines(1) = line([xbase(1), xbase(2)], [ybase(1), ybase(2)], [h, h]);
+topLines(1) = line([xbase(2), xbase(3)], [ybase(2), ybase(3)], [h, h]);
+topLines(1) = line([xbase(3), xbase(1)], [ybase(3), ybase(1)], [h, h]);
+
+% Kolumny
+for i = 1:3
+	colLines(i) = line([xbase(i), xbase(i)], [ybase(i), ybase(i)], [0, h]);
+end
+
+view(125, 10);
+
+% Kossel: 0..25k, 25k is bottom
+
+dont = 0;
+for t = 1:600
+	xc = sin(2*pi/60 *t);
+	yc = cos(2*pi/60 *t);
+	zc = t/100; % h/4 + sin(2*pi/120 * t);
+	for i = 1:3
+		dx = xbase(i) - xc;
+		dy = ybase(1) - yc;
+		z(i) = zc + sqrt(Rpret*Rpret - dx*dx - dy*dy);
+		if(z(i) > h)
+			dont = 1;
+		end
+	end
+	if(~dont)
+		for i = 1:3
+			set(arms(i), 'XData', [xbase(i), xc], 'YData', [ybase(i), yc], 'ZData', [z(i), zc]);
+			toSend(i) = round(25000 - (z(i)/h) * 25000);
+		end
+	end
+	str = sprintf('x %d y %d z %d\n', toSend(1), toSend(2), toSend(3));
+	fprintf(s, str);
+	drawnow();
+	pause(1);
+end
+
+% fclose(s);
